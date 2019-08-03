2d1b68b1b123b93c6528742d984bcf972071b73f
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-03 17:03:45 +0900

    first commit

---
+++ marubatuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marubatuApp
+//
+//  Created by VERTEX21 on 2019/08/03.
+//  Copyright © 2019 k-kougi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
